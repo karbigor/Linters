53cbdd5696b9346ad45822907109215909dbdfee
karbigor <122197991+karbigor@users.noreply.github.com>
2025-10-04 15:44:34 +0900

    Update models.py

--- module_29_testing/hw/flaskr/models.py
+++ module_29_testing/hw/flaskr/models.py
@@ -39,12 +39,13 @@ class Client_Parking(db.Model):
     __tablename__ = "client_parking"
     __table_args__ = (
       db.UniqueConstraint('client_id', 'parking_id',
-                          name='unique_client_parking'),)
+                          name='unique_client_parking'),
+    )
     id = db.Column(db.Integer, primary_key=True)
-    client_id = db.Column(db.Integer, db.ForeignKey('clients.id'))
-    client = db.relationship('Client', backref='client_parking')
-    parking_id = db.Column(db.Integer, db.ForeignKey('parkings.id'))
-    parking = db.relationship('Parking', backref='client_parking')
+    client_id = db.Column(db.Integer, db.ForeignKey("clients.id"))
+    client = db.relationship("Client", backref="client_parking")
+    parking_id = db.Column(db.Integer, db.ForeignKey("parkings.id"))
+    parking = db.relationship("Parking", backref="client_parking")
     time_in = db.Column(db.DateTime, nullable=False)
     time_out = db.Column(db.DateTime)
 
